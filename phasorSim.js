4e576ef6fa9469ba76d525ce2fb76628ac064f95
Paul T <PaulTorry@users.noreply.github.com>
2022-12-15 22:42:03 +0000

    streamline functions and add history + minor formatting

--- phasorSim.js
+++ phasorSim.js
@@ -3,9 +3,11 @@ import { Vec } from './Vec.js'
 // import { drawLine } from './drawFunctions.js'
 // import { drawForground, drawBackground } from './View/drawView.js'
 
-console.log('start')
+console.log('st  art')
 const phasors = [Vec.unitY.scale(100), Vec.unitX.scale(100)]
-let wavelength = 20;
+const hist = []
+let trace = false
+let wavelength = 20
 const canvas = document.querySelector('#screen')
 const cx = canvas.getContext('2d')
 // let dropdown = document.getElementById('wavelengthSlide')
@@ -15,14 +17,21 @@ const settings = {
   smallscreen: false
 }
 const pos = {
-  componentXY: new Vec(200, 200),
-  resultXY: { x: 300, dx: 5 }
+  p1: new Vec(150, 200),
+  g1: new Vec(300, 200),
+  p2: new Vec(150, 400),
+  g2: new Vec(300, 400),
+  p3: new Vec(150, 600),
+  g3: new Vec(300, 600)
+  // componentXY: new Vec(200, 200),
+  // resultXY: { x: 300, dx: 5 }
 }
+console.log(0, 200, phasors[1].mag, phasors[0].phase, 400, 20, 'lightgrey')
 
-const phasorBase = (n = 2) => { return pos.componentXY.scaleXY(1, n) }
+// const phasorBase = (n = 2) => { return pos.componentXY.scaleXY(1, n) }
 
-console.log(pos.componentXY)
-console.log(...pos.componentXY)
+// console.log(pos.componentXY)
+// console.log(...pos.componentXY)
 
 addEventListeners()
 requestAnimationFrame(animateIt)
@@ -31,18 +40,35 @@ update()
 function drawForground (c, phasors, pos) {
   c.fillStyle = 'lightgrey'
   c.strokeStyle = 'black'
-  drawLine(c, ...phasorBase(1), ...phasors[0])
-  drawLine(c, ...phasorBase(2), ...phasors[1])
-  c.strokeStyle = 'red'
-  drawLine(c, ...phasorBase(3), ...phasors[0])
+  drawLine(c, ...pos.p1, ...phasors[0])
+  drawLine(c, ...pos.p2, ...phasors[1])
   c.strokeStyle = 'green'
-  drawLine(c, ...phasorBase(3).add(phasors[0]), ...phasors[1])
+  drawLine(c, ...pos.p3, ...phasors[1])
+  c.strokeStyle = 'red'
+  drawLine(c, ...pos.p3.add(phasors[1]), ...phasors[0])
   c.strokeStyle = 'black'
-  drawLine(c, ...phasorBase(3).add(phasors[0]).add(phasors[1]), ...phasors[1].add(phasors[0]).invert())
-  // console.log(0, 200, phasors[1].mag, phasors[1].theta, 400, 20, 'lightgrey');
-  drawSine(...phasorBase(1), phasors[0].mag, phasors[0].phase, 800, wavelength, 'red')
-  drawSine(...phasorBase(2), phasors[1].mag, phasors[1].phase, 800, 20, 'green')
-  drawResult (...phasorBase(3), phasors[1].mag, 20, phasors[1].phase, phasors[0].mag, wavelength, phasors[0].phase, 800,  'black')
+  const finalPhasor = phasors[0].add(phasors[1])
+  drawLine(c, ...pos.p3.add(finalPhasor), ...finalPhasor.invert())
+
+  const func1 = transformFunc(Math.sin, phasors[0].mag, 1 / wavelength, phasors[0].phase)
+  const func2 = transformFunc(Math.sin, phasors[1].mag, 1 / 20, phasors[1].phase)
+
+  iteratePath(...pos.g1, 800, 'red', func1)
+  iteratePath(...pos.g2, 800, 'green', func2)
+  iteratePath(...pos.g3, 800, 'black', (x) => func1(x) + func2(x))
+
+  if (trace) {
+    drawHistory(c)
+    if (hist.length < 2000) hist.push(finalPhasor)
+  }
+  // console.log(hist)
+}
+
+function drawHistory (c) {
+  cx.strokeStyle = 'grey'
+  hist.forEach(
+    (v, i, a) => drawLine(c, ...a[i].add(pos.p3), ...a[i].subtract(a[Math.max(i - 1, 0)]))
+  )
 }
 
 function drawBackground (c, pos) {
@@ -52,35 +78,20 @@ function drawBackground (c, pos) {
 }
 
 function dragEvent (mouseCoords, b) {
-  phasors[0] = b.subtract(pos.componentXY)
+  phasors[0] = b.subtract(pos.p1)
   update()
 }
 
-function drawSine (x1, y1, A, theta, length, wl, color = 'black') {
-  let fun = (xxx) => A * Math.sin(xPosToTheta(xxx))
-  function xPosToTheta (x) {
-    return (x - 0) / (wl) - theta
-  }
-  iteratePath(x1, y1, length, color, fun)
-}
-
-function drawResult (x1, y1, A, wl, theta, A2, wl2, theta2, length, color = 'black') {
-  let fun = (xxx) => (A * Math.sin(xPosToTheta1(xxx))) + (A2 * Math.sin(xPosToTheta2(xxx)))
-  function xPosToTheta1 (x) {
-    return (x - 0) / (wl) - theta
-  }
-  function xPosToTheta2 (x) {
-    return (x - 0) / (wl2) - theta2
-  }
-  iteratePath(x1, y1, length, color, fun)
+function transformFunc (func, A = 1, B = 1, C = 0, D = 0) {
+  return (x) => A * func(B * x + C) + D
 }
 
 function iteratePath (x1, y1, length, color = 'black', func = Math.sin) {
   if (color) { cx.strokeStyle = color }
   cx.beginPath()
-  cx.moveTo(x1, y1)
+  cx.moveTo(x1, y1 + func(0))
   for (var xx = x1; xx <= x1 + length; xx += 1) {
-    cx.lineTo(xx, y1 + func(xx))
+    cx.lineTo(xx, y1 + func(xx - x1))
   }
   cx.stroke()
 }
@@ -99,8 +110,9 @@ function addEventListeners () {
   let mouseCoords
 
   document.addEventListener('keypress', (e) => {
-     if (e.key === '=') {wavelength++}
-     if (e.key === '-') {wavelength--}
+    if (e.key === '=') { wavelength++ }
+    if (e.key === '-') { wavelength-- }
+    if (e.key === 'h') { trace = !trace; hist.length = 0 }
     update(true)
   })
 
@@ -144,10 +156,10 @@ function animateIt (time, lastTime) {
     // if (ray.normalisedResultant.phase > newRay.normalisedResultant.phase) {
     //   intensity.addIntensity(ray, undefined, settings.mirror)
     // }
-     // phasors.forEach((v, i, a) => { phasors[i] = phasors[i].rotate(-0.01*wavelength/20) })
+    // phasors.forEach((v, i, a) => { phasors[i] = phasors[i].rotate(-0.01) })
+    phasors[0] = phasors[0].rotate(0.02 * 20 / wavelength)
+    phasors[1] = phasors[1].rotate(0.02)
 
-    phasors[0] = phasors[0].rotate(-0.01*20/wavelength) 
-    phasors[1] = phasors[1].rotate(-0.01) 
     update()
   }
 
